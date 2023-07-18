47a84473cd29f9ec73235b6726e7f9ac641f834f
veepthy suresh <109614794+VEEPTHY-SURESH@users.noreply.github.com>
2023-07-18 13:16:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let mybutton=document.getElementById("btn-back-to-top");window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop>20||document.documentElement.scrollTop>20){document.getElementById("navbar").style.top="0";mybutton.style.display="block"}else{document.getElementById("navbar").style.top="-75px";mybutton.style.display="none"}}
+mybutton.addEventListener("click",backToTop);function backToTop(){document.body.scrollTop=0;document.documentElement.scrollTop=0}
+
+
+
+
+
